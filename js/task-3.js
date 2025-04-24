fadb98b7173f85d1be766d26f8d3283956df7c10
Anastasiia K <vandyshevaa19@gmail.com>
2025-04-24 17:27:55 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
-  
+
   changeUsername(newName) {
     this.username = newName;
   },
 
   updatePlayTime(hours) {
     this.playTime += hours;
+  },
+
+  getInfo() {
+    return `Username: ${this.username}, Playtime: ${this.playTime}`;
   }
 };
 
